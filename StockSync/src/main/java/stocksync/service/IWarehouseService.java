267996fc10a9a405bfd25d4cd186f268bd3ca0be
Vincent Li <vfli@wisc.edu>
2024-03-10 12:27:25 -0500

    Vl/thymeleaf

---
+++ StockSync/src/main/java/stocksync/service/IWarehouseService.java
@@ -0,0 +1,9 @@
+package stocksync.service;
+
+import java.util.List;
+import stocksync.model.Warehouse;
+
+public interface IWarehouseService {
+    public abstract void createWarehouse(Warehouse newWh);
+    public abstract List<Warehouse> getWarehouses();
+}
